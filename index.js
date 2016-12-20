d11f039fda1b23b0998061ca8032ecd0adecd60a
Dewitt Buckingham <dj729@comcast.net>
2016-12-20 14:12:48 -0800

    User Model

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const mongoose = require('mongoose');
 
+const models = require('./models');
+const config = require('./config');
 
 /**
  * Mmm, Yes, Quite - An entertainment podcast created by Dewitt and Bjorn
  * @module myq
  */
+
+console.log('Going up...');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.MONGOOSE_URL);
+
