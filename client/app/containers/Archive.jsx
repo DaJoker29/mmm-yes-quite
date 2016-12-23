2f8f4b5e0e0fe9e8839e79c93bc39616c132e98a
Dewitt Buckingham <dj729@comcast.net>
2016-12-22 20:23:47 -0800

    Add Functional Navigation

---
+++ client/app/containers/Archive.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Archive extends Component {
+  render() {
+    return <h3>{'How ya doin\'!'}</h3>;
+  }
+}
+
+export default Archive;
--- client/app/index.jsx
+++ client/app/index.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import Home from './containers/Home';
+import Archive from './containers/Archive';
 import MainLayout from './layouts/main';
 
 render((
   <Router history={browserHistory}>
     <Route component={MainLayout}>
       <Route path="/" component={Home} />
+      <Route path="/archive" component={Archive} />
+      <Route path="/store" component={Archive} />
+      <Route path="/subscribe" component={Archive} />
     </Route>
   </Router>
 ), document.getElementById('app'));
