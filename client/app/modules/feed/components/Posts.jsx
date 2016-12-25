e1c5e19100e8c03d209e3094fb4c32b787e37b58
Dewitt Buckingham <dj729@comcast.net>
2016-12-24 19:43:10 -0800

    Post component

--- client/app/modules/feed/components/Posts.jsx
+++ client/app/modules/feed/components/Posts.jsx
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import moment from 'moment';
 import { fetchPostsIfNeeded } from '../actions';
+import Post from './Post';
 
 class Posts extends Component {
   componentDidMount() {
@@ -14,9 +14,7 @@ class Posts extends Component {
 
     return (
       <div>
-        {
-          feed.map(post => (renderPost(post)))
-        }
+        { feed.map(p => <Post key={p.id} post={p} />) }
       </div>
     );
   }
@@ -35,47 +33,3 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(Posts);
-
-function renderPost(post) {
-  const { type } = post;
-  let icon = '';
-  let color = '';
-
-  switch (type) {
-    case 'episode':
-      icon = 'microphone';
-      color = 'success';
-      break;
-    case 'article':
-      icon = 'file-text-o';
-      color = 'danger';
-      break;
-    case 'video':
-      icon = 'video-camera';
-      color = 'primary';
-      break;
-    default:
-      icon = 'question-circle';
-      color = 'warning';
-      break;
-  }
-
-  return (
-    <div key={post.id} class={`card card-outline-${color} text-white`}>
-      <div class="card-block">
-        <h4 class="card-title"><i class={`fa fa-${icon} text-${color} mr-1`} />{ post.title }<small class="text-muted float-xs-right"><i class="fa fa-minus" /> Remove from Playlist</small></h4>
-        <p class="card-subtitle text-muted">{ moment(post.date).format('MMMM Do, YYYY') }</p>
-      </div>
-      { 'video' === type ? 
-        <div class="card-block embed-responsive embed-responsive-16by9">
-          <iframe width="560" height="315" src={post.media} frameBorder="0" allowFullScreen />
-        </div>
-          : ''
-      }
-      <div class="card-block">
-        <p class="card-text">{ post.teaser }</p>
-        <p class="card-text hidden-xs-up">{ post.content }</p>
-      </div>
-    </div>
-  );
-}
