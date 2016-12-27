491eb54074e5a56950ee5b67aac6ed599d4b4c80
Dewitt Buckingham <dj729@comcast.net>
2016-12-26 17:27:03 -0800

    Style Input Range

--- client/app/modules/player/components/AudioProgress.jsx
+++ client/app/modules/player/components/AudioProgress.jsx
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 
 const AudioProgress = ({ elapsed, length }) => (
-  <progress id="audio-timeline" class="mt-1 mb-0 audio-timeline progress progress-success" value={`${100 / Math.floor(length / elapsed)}`} max="100" />
+  <input type="range" id="audio-timeline" class="mt-1 mb-0 audio-timeline progress progress-success" value={`${100 / Math.floor(length / elapsed)}`} max="100" />
 );
 
 AudioProgress.propTypes = {
